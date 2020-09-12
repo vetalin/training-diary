0aae960c53bf298686f764c9afc831e15f413a38
vetalin <vetal-frontend@yandex.ru>
2020-09-12 15:12:35 +0300

    add styled-components, add material-ui kit

---
+++ src/components/App/App.styled.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+export const AppContainer = styled.div`
+  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+`
---
+++ src/components/App/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as S from './App.styled'
+
+export const App = () => {
+  return (
+    <S.AppContainer>
+      Test text
+    </S.AppContainer>
+  )
+}
