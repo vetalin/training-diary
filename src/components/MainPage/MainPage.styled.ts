e7dc851d648e7187f09d44ce489ec202d9cdbba6
vetalin <vetal-frontend@yandex.ru>
2020-12-27 17:44:10 +0300

    add mainPage styled component

---
+++ src/components/MainPage/MainPage.styled.ts
@@ -0,0 +1,3 @@
+import styled from 'styled-components'
+
+export const MainPageContainer = styled.div``
--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { CreateTraining } from 'components/CreateTraining/CreateTraining'
 import { TrainingList } from 'components/TrainingList/TrainingList'
+import * as S from './MainPage.styled'
 
 export const MainPage: React.FC = () => {
   return (
-    <React.Fragment>
+    <S.MainPageContainer>
       <CreateTraining/>
       <TrainingList/>
-    </React.Fragment>
+    </S.MainPageContainer>
   )
 }
